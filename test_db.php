ff1b592d2ec69ad8b4bdccfb57ff3f185babb9ab
kizenga11 <kizengagodlove5@gmail.com>
2026-05-11 14:39:50 -0700

    Force error display in config.php and simplify test_db.php

--- test_db.php
+++ test_db.php
@@ -4,7 +4,7 @@ try {
     $pdo = new PDO(
         "mysql:host=mysql.railway.internal;dbname=railway;charset=utf8mb4",
         "root",
-        "YOUR_PASSWORD_HERE"
+        "BQuvHkfEqQPPdMfTADAZGZzRlafWGsSU"
     );
 
     echo "DB CONNECTED OK";
