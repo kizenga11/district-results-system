4b523af8df63d5bdf4d1c40281568866964498dd
kizenga11 <kizengagodlove5@gmail.com>
2026-05-11 12:49:24 -0700

    Enable temporary error reporting on login page for debugging

--- login.php
+++ login.php
@@ -1,5 +1,10 @@
 <?php
 
+// Temporarily enable error display for login page debugging.
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 declare(strict_types=1);
 
 require_once __DIR__ . '/includes/layout.php';
